1315fa2345d84c11d08c146f8b5e86d9ff82b504
Julie Jiang <Julie.Jiang@emc.com>
2019-04-17 03:46:22 +0000

    Handle ABORT result and format display
    1. render with "aborted" instead of chart when the result is aborted
    2. add css style for the jobTestResult widget

--- src/components/JobTestResult.jsx
+++ src/components/JobTestResult.jsx
@@ -3,13 +3,14 @@ import React, { Component, PropTypes } from 'react'; // eslint-disable-line no-u
 import reactMixin                      from 'react-mixin';
 import { ListenerMixin }               from 'reflux';
 import Mozaik                          from 'mozaik/browser';
+const { Pie } = Mozaik.Component;
 
 class JobTestResult extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-           result : null 
+           result : {}
         };
     }
 
@@ -27,19 +28,68 @@ class JobTestResult extends Component {
     }
 
     render() {
-        const fullDisplayName = result["fullDisplayName"];
-        testResultNode = (
-            <div>
-                Test
+        const { result } = this.state;
+        const { job } = this.props;
+        let fullDisplayName = '';
+        let testResultDateStr = '';
+        let testResultNode = (
+            <div className="jenkins__job-test-result__loading">
+                Loading...
             </div>
         );
+
+        if (result && result.actions) {
+            let testResultDate = new Date(result.timestamp);
+            testResultDateStr = testResultDate.toLocaleString(
+                'en-GB', { timeZone: 'Asia/Shanghai' });
+            fullDisplayName = result.fullDisplayName;
+            if (result.result == 'ABORTED') {
+                testResultNode = (
+                    <div className="jenkins__job-test-result__aborted">
+                        ABORTED
+                    </div>
+                );
+            } else {
+                const actions = result.actions;
+                let testResult = [
+                    {id: 'fail', label: 'fail', color: '#B80000', count: 0}, 
+                    {id: 'skip', label: 'skip', color: '#FCCB00', count: 0},
+                    {id: 'success', label: 'success', color: '#2A8B2B', count: 0}
+                ];
+                let totalCount = 0;
+                for (const action of actions) {
+                    if(action._class == 'hudson.tasks.junit.TestResultAction') {
+                        testResult[0]['count'] = action.failCount;
+                        testResult[1]['count'] = action.skipCount;
+                        testResult[2]['count'] = action.totalCount - action.failCount - action.skipCount;
+                        totalCount = action.totalCount;
+                        break;
+                    }
+                }
+
+                testResultNode = (
+                    <Pie data={testResult} count={totalCount} countLabel={'tests'} innerRadius={0.7}/>
+                );
+            }
+        }
         return(
             <div>
                 <div className="widget__header">
-                    { fullDisplayName }
+                    Jenkins { job } Last Completed Build Result
                     <i className="fa fa-bug" />
                 </div>
                 <div className="widget__body">
+                    <div className="jenkins__job-test-result__title">
+                        <span className="jenkins__job-test-result__title__desc">
+                            Last Completed Test:
+                        </span>
+                        <span className="jenkins__job-test-result__title__name">
+                            { fullDisplayName }
+                        </span>
+                    </div>
+                    <div className="jenkins__job-test-result__time">
+                        { testResultDateStr }
+                    </div>
                     {testResultNode}
                 </div>
             </div>
